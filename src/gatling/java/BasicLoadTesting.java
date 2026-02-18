3f3ed45662662ea9c24d9c1e27f968c6f10d1434
Anton Romanov <a.romanov@ulstu.ru>
2026-02-18 11:25:10 +0400

    Fix gatling test for github actions

--- src/gatling/java/BasicLoadTesting.java
+++ src/gatling/java/BasicLoadTesting.java
@@ -33,7 +33,7 @@ public class BasicLoadTesting extends Simulation {
                             """)));
     {
         setUp(
-                indexLoadTest.injectOpen(atOnceUsers(10))
+                indexLoadTest.injectOpen(atOnceUsers(100))
         ).protocols(httpProtocol)
                 .assertions(
                         global().successfulRequests().percent().gt(80.0)
